d188afd951bd3c9446be79715b9e8d261bd70255
Andreas Laub <andreas@laub-home.de>
2025-10-16 13:34:41 +0200

    added e2e cirrussearch and elasticsearch

--- resources/mediawiki/generate-elasticindex.sh
+++ resources/mediawiki/generate-elasticindex.sh
@@ -4,3 +4,5 @@ cd /var/www/html
 
 php maintenance/run.php ./extensions/CirrusSearch/maintenance/UpdateSearchIndexConfig.php --startOver
 php maintenance/run.php ./extensions/CirrusSearch/maintenance/ForceSearchIndex.php
+php maintenance/run.php ./extensions/CirrusSearch/maintenance/UpdateSearchIndexConfig.php
+php maintenance/run.php ./extensions/CirrusSearch/maintenance/UpdateSuggesterIndex.php
